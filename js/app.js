a2536cb91f44ccd0b1e59a537066d16c7663d757
Carol <carolinav.st@gmail.com>
2024-01-03 14:34:03 -0300

    Conclusão com função reiniciar

--- js/app.js
+++ js/app.js
@@ -40,3 +40,9 @@ function embaralha(lista) {
             [lista[indiceAleatorio], lista[indice - 1]];
     }
 }
+
+function reiniciar() {
+    amigos = [];
+    document.getElementById("lista-amigos").innerHTML = "";
+    document.getElementById("lista-sorteio").innerHTML = "";
+}
