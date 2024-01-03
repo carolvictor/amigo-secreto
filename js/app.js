f7233dca2e05883c08e9a548af9cdf45b1c39821
Carol <carolinav.st@gmail.com>
2024-01-02 21:08:42 -0300

    conclusão função sortear

--- js/app.js
+++ js/app.js
@@ -1,16 +1,42 @@
+let amigos = [];
+
 function adicionar() {
     let nomeAmigo = document.getElementById("nome-amigo");
     let listaAmigos = document.getElementById("lista-amigos");
-
+    amigos.push(nomeAmigo.value);
     if(listaAmigos.textContent == "") {
         listaAmigos.textContent = nomeAmigo.value;
     } else {
         listaAmigos.textContent = listaAmigos.textContent + ", " + nomeAmigo.value;
     }
-    nomeAmigo.value = "";
+    nomeAmigo.value = ""; // para limpar o campo
 
 }
 
 function sortear() {
-    let listaSorteio = document.getElementById("lista-sorteio")
+    embaralha(amigos);
+    let listaSorteio = document.getElementById("lista-sorteio");
+
+        for (let i = 0; i < amigos.length; i++) {
+
+            if(i == amigos.length - 1){ //que significa que o indice chegou no final do array
+                listaSorteio.innerHTML = listaSorteio.innerHTML + amigos[i] + "--> " + amigos[0] + "<br>";
+            } else {
+                listaSorteio.innerHTML = listaSorteio.innerHTML + amigos[i] + "--> " + amigos[i+1] + "<br>";
+            //amigos[i+1] é o amigo que está na próxima posição
+            //<br> para ir para a linha abaixo no html
+            }      
+    }    
+}
+
+function embaralha(lista) {
+
+    for (let indice = lista.length; indice; indice--) {
+
+        const indiceAleatorio = Math.floor(Math.random() * indice);
+
+        // atribuição via destructuring
+        [lista[indice - 1], lista[indiceAleatorio]] = 
+            [lista[indiceAleatorio], lista[indice - 1]];
+    }
 }
